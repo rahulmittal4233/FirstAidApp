1df53b529310087de172a6ea3ac3615c2b6b888d
Rahul Mittal <46190008+rahulmittal4233@users.noreply.github.com>
2019-08-08 19:49:21 +0530

    Add files via upload

---
+++ app/src/main/java/org/indianredcross/firstaid/Data/Contacts.java
@@ -0,0 +1,26 @@
+//Data Models for favourite contacts list
+package org.indianredcross.firstaid.Data;
+
+
+public class Contacts {
+    private String name;
+    private String number;
+    public Contacts() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
